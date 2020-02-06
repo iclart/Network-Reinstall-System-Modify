94e9392289bf767c7efe91469068af2f244ac413
xinyu <xinyu@ifreetion.com>
2020-02-06 15:20:19 +0800

    Update LVM_DISK_Auto_Partition_Tools.sh

--- CoreShell/LVM_DISK_Auto_Partition_Tools.sh
+++ CoreShell/LVM_DISK_Auto_Partition_Tools.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "------------------------------------------------------------------------------------------------------------------------------------------------------"
-echo -e "\033[33m LVM DISK Auto Partition Tools Version: 1.0.2 (20190622) \033[0m |  Technical support provided by [ https://cxthhhhh.com ] "
+echo -e "\033[33m LVM DISK Auto Partition Tools Version: centos-1.1.0 (20200206-6e18f66c35b8) \033[0m |  Origin Author: [ https://cxthhhh.com ] "
+echo -e "\033[33m Author: iClart \033[0m"
 echo "------------------------------------------------------------------------------------------------------------------------------------------------------"
 echo -e "\033[33m 8888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888 \033[0m"
 echo -e "\033[33m 8888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888 \033[0m"
@@ -23,12 +24,12 @@ echo -e "\033[33m 888   888888    88    888    8888   8888   88888   88888888   
 echo -e "\033[33m 88    8888888   888          8888     888         888888888   888888888          888   8888888     88   88888     88   8888         8888   8888   88 \033[0m"
 echo -e "\033[33m 8888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888 \033[0m"
 echo "------------------------------------------------------------------------------------------------------------------------------------------------------"
-echo -e "\033[33m LVM DISK Auto Partition Tools Version: 1.0.2 (20190622) \033[0m |  Technical support provided by [ https://cxthhhhh.com ] "
+echo -e "\033[33m LVM DISK Auto Partition Tools Version: centos-1.1.0 (20200206-6e18f66c35b8) \033[0m |  Origin Author: [ https://cxthhhh.com ] "
 echo "------------------------------------------------------------------------------------------------------------------------------------------------------"
-echo "Analyze Disk type (Virtual/Physical)......"
+echo "Analyze Disk type (PV/SATA/PATA/SAS/SCSI/NVMe)......"
 sleep 5
 if [ -b /dev/vda1 ]; then
-echo "Disk type: Virtual Disk"
+echo "Disk type: VirtIO Paravirtualization Disk"
 echo "Recognition completed, waiting for disk response."
 echo "Automatic partitioning will take more than 2 minutes to complete."
 sleep 3s
@@ -59,7 +60,7 @@ echo "In the partition (5/5), waiting for the disk response will take more than 
 resize2fs -p /dev/mapper/centos-root
 sleep 6s
 elif [ -b /dev/xvda1 ]; then
-echo "Disk type: Virtual Disk"
+echo "Disk type: Xen Paravirtualization Disk"
 echo "Recognition completed, waiting for disk response."
 echo "Automatic partitioning will take more than 2 minutes to complete."
 sleep 3s
@@ -78,10 +79,72 @@ echo "In the partition (1/5), waiting for the disk response will take more than 
 partprobe
 sleep 20s
 echo "In the partition (2/5), waiting for the disk response will take more than 15 seconds."
-pvcreate /dev/vda3
+pvcreate /dev/xvda3
 sleep 11s
 echo "In the partition (3/5), waiting for the disk response will take more than 15 seconds."
-vgextend centos /dev/vda3
+vgextend centos /dev/xvda3
+sleep 11s
+echo "In the partition (4/5), waiting for the disk response will take more than 15 seconds."
+lvextend -l +100%FREE /dev/mapper/centos-root
+sleep 11s
+echo "In the partition (5/5), waiting for the disk response will take more than 10 seconds."
+resize2fs -p /dev/mapper/centos-root
+sleep 6s
+elif [ -b /dev/hda1 ]; then
+echo "Disk type: PATA (IDE) Disk"
+echo "Recognition completed, waiting for disk response."
+echo "Automatic partitioning will take more than 2 minutes to complete."
+sleep 3s
+echo "n
+p
+3
+
+
+t
+3
+8e
+w
+" | fdisk /dev/hda
+sleep 10s
+echo "In the partition (1/5), waiting for the disk response will take more than 30 seconds."
+partprobe
+sleep 20s
+echo "In the partition (2/5), waiting for the disk response will take more than 15 seconds."
+pvcreate /dev/hda3
+sleep 11s
+echo "In the partition (3/5), waiting for the disk response will take more than 15 seconds."
+vgextend centos /dev/hda3
+sleep 11s
+echo "In the partition (4/5), waiting for the disk response will take more than 15 seconds."
+lvextend -l +100%FREE /dev/mapper/centos-root
+sleep 11s
+echo "In the partition (5/5), waiting for the disk response will take more than 10 seconds."
+resize2fs -p /dev/mapper/centos-root
+sleep 6s
+elif [ -b /dev/nvme0n1 ]; then
+echo "Disk type: NVMe Disk"
+echo "Recognition completed, waiting for disk response."
+echo "Automatic partitioning will take more than 2 minutes to complete."
+sleep 3s
+echo "n
+p
+3
+
+
+t
+3
+8e
+w
+" | fdisk /dev/nvme0n1
+sleep 10s
+echo "In the partition (1/5), waiting for the disk response will take more than 30 seconds."
+partprobe
+sleep 20s
+echo "In the partition (2/5), waiting for the disk response will take more than 15 seconds."
+pvcreate /dev/nvme0n1p3
+sleep 11s
+echo "In the partition (3/5), waiting for the disk response will take more than 15 seconds."
+vgextend centos /dev/nvme0n1p3
 sleep 11s
 echo "In the partition (4/5), waiting for the disk response will take more than 15 seconds."
 lvextend -l +100%FREE /dev/mapper/centos-root
@@ -90,7 +153,7 @@ echo "In the partition (5/5), waiting for the disk response will take more than 
 resize2fs -p /dev/mapper/centos-root
 sleep 6s
 else
-echo "Disk type: Physical Disk"
+echo "Disk type: SATA Disk"
 echo "Recognition completed, waiting for disk response."
 echo "Automatic partitioning will take more than 2 minutes to complete."
 sleep 3s
